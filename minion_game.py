3f543c142e3a5e60f2113a0fee063efeb8ddf1a6
Xinyu Phoebe Liu <poeticphoebe@gmail.com>
2018-01-28 14:40:46 +0800

    Update minion_game.py

--- minion_game.py
+++ minion_game.py
@@ -1,4 +1,13 @@
-##
+# Minion game
+# Input: single line of input containing the string S that contain only uppercase letters: [A-Z]
+# Stuart has to make words starting with consonants.
+# Kevin has to make words starting with vowels. 
+# The game ends when both players have made all possible substrings. 
+# Output: the name of the winner and their score separated by a space. print "Draw" if draw
+
+# Optimization step: for a the substrings that start with s[i] are all scored to the same person, 
+# it does not matter what the substrings are.
+
 
 def minion_game(string):
     # your code goes here
